8fa5171ae83847bbae616e83475281be5a2189e9
aescuderof <antoescudero@gmail.com>
2021-06-25 13:54:09 -0400

    revision-1

---
+++ source/template/pages/form.php
@@ -0,0 +1,20 @@
+<?php
+
+	$to = "your email here";
+	$from = 'email';
+	$name = 'name';
+	$headers = "From: $from";
+	$subject = "You have a message.";
+
+	$fields = array();
+	$fields{"name"} = "First Name";
+	$fields{"email"} = "Email";
+	$fields{"phone"} = "Phone Numner";
+	$fields{"subject"} = "Subject";
+	$fields{"message"} = "Your Message";
+
+	$body = "Here is what was sent:\n\n"; foreach($fields as $a => $b){ $body .= sprintf("%20s:%s\n",$b,$_REQUEST[$a]); }
+
+	$send = mail($to, $subject, $body, $headers, $message);
+
+?>
